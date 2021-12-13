fc1f436d12d75604ba245c48815578674dbc4927
Adrian Kiva <akiva@mta.ca>
2021-12-13 18:39:26 -0500

    add counter to red when no chars remain

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,19 @@
+
+
 $(document).ready(function() {
   // --- our code goes here ---
   $('textarea').on("keyup", function(event) {
-    console.log($(this).val().length);
+    
     let chars = $(this).val().length;
-    $($(this)[0].parentNode[2]).val(140 - chars);
-    console.log($($(this)[0].parentNode[2]).val())
+    let counter = $($(this)[0].parentNode[2]);
+
+    counter.val(140 - chars);
+
+    let remaining =  counter.val();
+    let excess = counter[0].attributes[3];
+
+    if (remaining < 0) {
+      excess.value = true;
+    }
   })
 });
-//[0].parentNode[2]
-
-//[0].form.length
-// parent tweeta -> sibling tweetb -> child output --> val
