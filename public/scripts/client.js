c37821384c88200b137c98a33652ec8e84f3166c
Adrian Kiva <akiva@mta.ca>
2021-12-14 23:38:04 -0500

    create createTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+
+
+  const practiceTweet = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ };
+
+ const createTweetElement = (tweetObject) => {
+
+  const $tweet = $(
+    `  <article class="tweets">
+    <header>
+      <div class="profile">
+        <div class="icon"><img src="${tweetObject.user.avatars}"></div>
+        <div class="name">${tweetObject.user.name}</div>
+      </div>
+      <div>
+        <div class="username">${tweetObject.user.handle}</div>
+      </div>
+    </header>
+    <div class="content">${tweetObject.content.text}</div>
+    <footer>
+      <div class="age">
+        ${tweetObject.created_at} day(s) ago
+      </div>
+      <div class="social-icons">
+        <i class="fas fa-flag"></i> 
+        <i class="fas fa-retweet"></i> 
+        <i class="far fa-heart"></i></div>
+    </footer>
+  </article>`
+  )
+
+  return $tweet;
+ }
+
+console.log(createTweetElement(practiceTweet));
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
