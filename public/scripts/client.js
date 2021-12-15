73df7cf3ac0acb3667f974b7b26ae0ab202e718f
Adrian Kiva <akiva@mta.ca>
2021-12-15 00:15:50 -0500

    add render tweets functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,6 +18,30 @@ $(() => {
       },
     "created_at": 1461116232227
  };
+ const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
  const createTweetElement = (tweetObject) => {
 
@@ -48,9 +72,21 @@ $(() => {
   return $tweet;
  }
 
- const $tweet = createTweetElement(practiceTweet);
+ const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  const $tweetsContainer = $('#tweets-container')
+  $tweetsContainer.empty();
 
- $('#tweets-container').prepend($tweet);
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
+  }
+}
+
+ renderTweets(data);
+
 
 
 
